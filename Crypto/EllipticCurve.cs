b83e37272f4ebb45404feb9d605f79fdc949ff9a
Orfac <molodeckij.b.2017@fml31.ru>
2019-04-30 02:20:34 +0300

    Added elliptic curve class

---
+++ Crypto/EllipticCurve.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crypto
+{
+    internal class EllipticCurve
+    {
+        int _a;
+
+        int _b; 
+
+        public EllipticCurve(int a, int b)
+        {
+            _a = a;
+            _b = b;
+        }
+    }
+}

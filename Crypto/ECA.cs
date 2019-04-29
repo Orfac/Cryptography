c1c5f270af30fa7bbccde159640f0df46c1afe27
Orfac <molodeckij.b.2017@fml31.ru>
2019-04-30 02:21:20 +0300

    created elliptic curve algorythm class without implementation of decrpyt/encrypt

---
+++ Crypto/ECA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Crypto
+{
+    /// <summary>
+    /// An elliptic curve algorythm
+    /// </summary>
+    public class ECA : CryptoAlgorithm
+    {
+        EllipticCurve _curve;
+        public ECA(int a = -1, int b = 1)
+        {
+            _curve = new EllipticCurve(a,b);
+        }
+
+        public override byte[] Decrypt(byte[] encryptedData, byte[] key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] Encrypt(byte[] data, byte[] key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
